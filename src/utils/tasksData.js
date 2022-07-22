532f81a9bb8a57ea61f865a0aea9231504e16514
Anna Krasnovid <krasnovidanka@yandex.ru>
2022-07-22 21:06:11 +0300

    refactor, add redux

--- src/utils/tasksData.js
+++ src/utils/tasksData.js
@@ -2,18 +2,18 @@ const tasksData = [
   {
     id: '5o992iht1hb',
     task: 'Сделать тестовое',
-    checked: false
+    checked: false,
   },
   {
     id: 'zm7tzh08aam',
     task: 'Прекрасный код',
-    checked: true
+    checked: true,
   },
   {
     id: 'v6cze5l2qeo',
     task: 'Покрыть все тестами',
-    checked: false
-  }
-]
+    checked: false,
+  },
+];
 
 export default tasksData;
